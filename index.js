000cf270756e3e8feb79b50afb55997f77fb0efa
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-08-15 18:54:05 +1000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(task = "roller-skate") {
+  return `This Saturday, I want to ${task}!`;
+}
+
+const mondayWork = function (task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(wrapper = "*"){
+    return function(adj = "special"){
+    return `You are ${wrapper}${adj}${wrapper}!`
+    }
+
+    }
+
